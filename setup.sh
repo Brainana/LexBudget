d2b9b104d745d7c0dd2f30c4e69bcbe0f5cf4a41
Brainana <jerry.max.xu@gmail.com>
2024-02-03 22:56:33 -0500

    Update sed command to use the environment variable

--- setup.sh
+++ setup.sh
@@ -7,15 +7,14 @@ email = \"your-email@domain.com\"\n\
 
 echo "\
 [server]\n\
-headless = false\n\
-enableCORS=true\n\
+headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
 
-open_ai_api_key=`heroku config:get OPENAI_API_KEY`
-sed -i "s/your-api-key-here/$open_ai_api_key/g" ./.streamlit/secrets.toml
+sed -i "s/your-api-key-here/$OPENAI_API_KEY/" ./.streamlit/secrets.toml
 
-assistant_id=`heroku config:get ASSISTANT_ID`
-sed -i "s/your-assistant-id-here/$assisant_id/g" ./config.ini
+sed -i "s/your-assistant-id-here/$ASSISTANT_ID/" ./config.ini
+
 
 
