ef1c9aec2af9e038b85368502ecd5820cde47e74
Brainana <jerry.max.xu@gmail.com>
2024-03-24 14:13:16 -0400

    Reduce the copilot iframe width for mobile device

--- pages/5_Microsoft_Copilot.py
+++ pages/5_Microsoft_Copilot.py
@@ -3,4 +3,4 @@ import streamlit.components.v1 as components
 
 # embed copilot into streamlit webpage
 iframe_src = "https://copilot.microsoft.com/?showntbk=1"
-components.iframe(iframe_src, width=1200, height=800)
+components.iframe(iframe_src, width=350, height=800)
